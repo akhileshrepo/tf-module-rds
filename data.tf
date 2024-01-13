1b734995b8e221f2b04677a82a6b5c7dca541cd4
AKhilesh <kothapallyakhil123@gmail.com>
2024-01-13 09:45:53 +0530

    updated code

--- data.tf
+++ data.tf
@@ -6,6 +6,6 @@ data "aws_ssm_parameter" "master_password" {
   name = "rds.${var.env}.master_password"
 }
 
-data "aws_ssm_parameter" "database_username" {
-  name = "rds.${var.env}.database_username"
+data "aws_ssm_parameter" "database_name" {
+  name = "rds.${var.env}.database_name"
 }

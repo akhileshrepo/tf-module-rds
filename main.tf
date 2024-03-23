a4f7edd6c45706d2cb3bf9104f42588ba957fcf6
AKhilesh <kothapallyakhil123@gmail.com>
2024-03-23 18:53:52 +0530

    updated code

--- main.tf
+++ main.tf
@@ -1,7 +1,6 @@
 resource "aws_db_subnet_group" "main" {
   name       = "${local.name_prefix}-subnet-group"
   subnet_ids = var.subnet_ids
-
   tags = merge(local.tags, {Name = "${local.name_prefix}-subnet-group"})
 }
 

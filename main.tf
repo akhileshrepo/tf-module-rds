978797bd18ad8cc9264caf2ca522170afd500ee0
AKhilesh <kothapallyakhil123@gmail.com>
2024-08-26 11:14:50 +0530

    changes

--- main.tf
+++ main.tf
@@ -46,6 +46,8 @@ resource "aws_rds_cluster" "main" {
   vpc_security_group_ids = [aws_security_group.main.id]
   skip_final_snapshot     = var.skip_final_snapshot
   tags = merge(local.tags, { Name = "${local.name_prefix}-cluster" })
+  storage_encrypted = true
+  kms_key_id = var.kms_key_id
 }
 
 resource "aws_rds_cluster_instance" "cluster_instances" {
--- vars.tf
+++ vars.tf
@@ -13,3 +13,4 @@ variable "preferred_backup_window" {}
 variable "instance_count" {}
 variable "instance_class" {}
 variable "skip_final_snapshot" {}
+variable "kms_key_id" {}
